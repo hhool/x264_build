412a36f65cb29dc00ab7afd28a650c174c758795
hhool <hhool.student@gmail.com>
2021-01-22 20:24:56 +0800

    chore: init x264_build script

---
+++ README.md
@@ -0,0 +1,32 @@
+#
+## get code
+wget https://download.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20191217-2245.tar.bz2
+tar xvf x264-snapshot-20191217-2245.tar.bz2
+mv x264-snapshot-20191217-2245 src
+## build
+`windows`
+```
+ide vs2019 professional
+wget https://repo.msys2.org/distrib/x86_64/msys2-x86_64-20201109.exe
+Install and update msys environment
+https://www.msys2.org
+```
+`x86`
+
+![Image](doc/x86_native_win.png "x86 Native Tools Command Prompt for VS 2019")
+
+![Image](doc/msys2_x86_win.png "msys2 x86 console")
+
+`x64`
+
+![Image](doc/x64_native_win.png "x64 Native Tools Command Prompt for VS 2019")
+
+![Image](doc/msys2_x64_win.png "msys2 x64 console")
+
+`android`
+```
+download: android-ndk-r22 (linux)
+
+NDK_ROOT=path_to_android-ndk-r22 android_build.sh
+```
+
---
+++ android_build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+NDK_ROOT=/mnt/hgfs/project/x264/android-ndk-r22
+TOOLCHAIN=${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64
+API=22
+
+function x264_build
+{
+cd src
+echo "CC:"${CC}
+echo "CXX:"${CXX}
+echo "ADDITIONAL_CONFIGURE_FLAG:"${ADDITIONAL_CONFIGURE_FLAG}
+./configure --prefix=$PREFIX \
+--extra-cflags=${EXTRA_CFLAGS} \
+--host=${HOST} \
+--cross-prefix=${CROSS_PREFIX} \
+--sysroot=${TOOLCHAIN}/sysroot \
+${ADDITIONAL_CONFIGURE_FLAG} \
+--disable-cli --enable-shared --enable-static --enable-pic
+make clean
+make -j8
+make install
+cd ..
+}
+
+#x86
+export CC=${TOOLCHAIN}/bin/i686-linux-android${API}-clang
+export CXX=${TOOLCHAIN}/bin/i686-linux-android${API}-clang++
+PREFIX=../installed/android/x86
+EXTRA_CFLAGS=""
+ADDITIONAL_CONFIGURE_FLAG="--disable-asm"
+HOST=i686-linux-android
+CROSS_PREFIX=${TOOLCHAIN}/bin/i686-linux-android-
+x264_build
+
+#x86_64
+export CC=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang
+export CXX=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang++
+PREFIX=../installed/android/x86_64
+EXTRA_CFLAGS=""
+ADDITIONAL_CONFIGURE_FLAG=""
+HOST=x86_64-linux-android
+CROSS_PREFIX=${TOOLCHAIN}/bin/x86_64-linux-android-
+x264_build
+
+#arm64-v8a
+export CC=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang
+export CXX=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang++
+PREFIX=../installed/android/arm64-v8a
+EXTRA_CFLAGS=""
+ADDITIONAL_CONFIGURE_FLAG=""
+HOST=aarch64-linux-android
+CROSS_PREFIX=${TOOLCHAIN}/bin/aarch64-linux-android-
+x264_build
+
+#armeabi-v7a
+export CC=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang
+export CXX=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang++
+PREFIX=../installed/android/armeabi-v7a
+EXTRA_CFLAGS=""
+ADDITIONAL_CONFIGURE_FLAG=""
+HOST=armv7a-linux-android
+CROSS_PREFIX=${TOOLCHAIN}/bin/arm-linux-androideabi-
+x264_build
